ca5cc64751cebce2e20f3e304ded76a7c198c2e3
zchengb <zxchengb@163.com>
2020-08-06 18:08:02 +0800

    Modify to MAIN Method argument transfer.

--- jar-dependency-parse/README.md
+++ jar-dependency-parse/README.md
@@ -2,18 +2,16 @@
 
 ## 使用方法
 
-提取目录下的`jar-dependency-parse-1.0.0-jar-with-dependencies.jar`，随后执行下述Java命令提取Jar包依赖 **（仅支持Spring Boot框架依赖提取）** 。
+提取目录下的`parse-jar-with-dependencies.jar`，随后执行下述Java命令提取Jar包依赖 **（仅支持Spring Boot框架依赖提取）** 。
 
 ```shell
-# Java命令执行Jar包
-$ java -jar jar-dependency-parse-1.0.0-jar-with-dependencies.jar
-Please input JAR File path :
-e:\path\test-1.0.0-standalone.jar
-
-Found 95 dependencies in lib.
-
-OUT PUT Dependencies JSON in file: e:\path\test-1.0.0-standalone_dependencies_1596680844616.json
+# Java命令执行Jar包 （确保解析工具包与待解析JAR包位于同级目录下）
+$ java -jar parse-jar-with-dependencies.jar test
+# 运行结果
+Parsing test.jar...
+Found 5 dependencies in lib.
+SUCCESS OUTPUT Dependencies JSON in file: test_dependencies_1596708223404.json
 ```
 
-随后可在` e:\path\test-1.0.0-standalone_dependencies_1596680844616.json`路径下查看生成的JSON文件。
+随后可在同路径下查看生成的JSON文件。
 
--- jar-dependency-parse/src/main/java/com/jfeat/util/FileUtils.java
+++ jar-dependency-parse/src/main/java/com/jfeat/util/FileUtils.java
@@ -3,11 +3,14 @@ package com.jfeat.util;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * @author zxchengb
@@ -71,6 +74,31 @@ public class FileUtils {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+        return null;
+    }
+
+    /**
+     * 通过jar包文件解压，获取其依赖包(lib)并生成依赖集合
+     *
+     * @param jarFile 目标JAR包
+     * @return java.util.List<java.lang.String>
+     */
+    public static List<String> getDependencies(File jarFile) {
+        List<String> dependencies = new ArrayList<>();
+        // 不解压读取压缩包中的文件内容
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(jarFile))) {
+            ZipEntry zipEntry;
+            // 循环遍历压缩包内文件对象
+            while ((zipEntry = zis.getNextEntry()) != null) {
+                String name = zipEntry.getName();
+                if (name.startsWith(FileUtils.LIB_JAR_DIR) && name.endsWith(FileUtils.JAR_SUFFIX)) {
+                    dependencies.add(name.replaceFirst(FileUtils.LIB_JAR_DIR, ""));
+                }
+            }
+            return dependencies;
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
         return null;
     }
